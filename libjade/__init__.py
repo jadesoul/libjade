b366c2f0a125719088e8b4950bf9ad2eaf861144
jadesoul <wslgb2006@gmail.com>
2012-06-27 15:29:09 +0800

    add some scripts
    Signed-off-by: jadesoul <wslgb2006@gmail.com>

--- libjade/__init__.py
+++ libjade/__init__.py
@@ -3,6 +3,7 @@
 '''
 author:	jadesoul
 datetime:	2011-11-8
+now move to git
 '''
 
 from common import *
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from os import listdir
 
-setup(name='libjade',
+setup(
+	  name='libjade',
       version='1.0',
       description='Jadesoul\'s Python Distribution Utilities',
       author='Jaden Wu',
       author_email='wslgb2006@gmail.com',
       url='http://jadesoul.sinaapp.com/',
-      packages=['libjade']
-     )
+	  license='Python Software Foundation License',
+      packages=['libjade'],
+	  scripts=['scripts/'+i for i in listdir('scripts')],
+)
