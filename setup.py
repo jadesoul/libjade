04083f4731b39cb4662c311b95944bfd15100ae1
jadesoul <wslgb2006@gmail.com>
2012-06-27 14:36:09 +0800

    add a setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='libjade',
+      version='1.0',
+      description='Jadesoul\'s Python Distribution Utilities',
+      author='Jaden Wu',
+      author_email='wslgb2006@gmail.com',
+      url='http://jadesoul.sinaapp.com/',
+      packages=['libjade']
+     )
