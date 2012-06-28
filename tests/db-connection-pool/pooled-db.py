2597261a4d494f71ec0f19651f6e7284aaca55a4
jadesoul <wslgb2006@gmail.com>
2012-06-28 11:52:35 +0800

    no message Signed-off-by: jadesoul <wslgb2006@gmail.com>

---
+++ tests/db-connection-pool/pooled-db.py
@@ -0,0 +1,30 @@
+#coding:utf8
+
+import threading,time,datetime, MySQLdb, DBUtils
+from DBUtils.PooledDB import PooledDB
+
+pool = PooledDB(MySQLdb,100,50,100,490,False,
+	host='localhost',user='root',passwd='gbsoft',db='test',
+	charset='utf8')
+
+class MyThread(threading.Thread, object):
+	
+	def __init__(self,threadName):
+		self.conn = pool.connection()
+		threading.Thread.__init__(self,name=threadName)
+
+	def run(self):
+		cursor=self.conn.cursor()
+		print "hello--->",self.getName()
+		time.sleep(10)  
+	
+	def __del__(self):
+		self.conn.close()  
+		self.conn = None   
+
+if __name__=='__main__':
+	for i in range(100):  
+		obj = MyThread(str(i))  
+		obj.start() 
+
+
