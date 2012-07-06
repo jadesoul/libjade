996fbf62913edde2b69a4546f6e6ac1f80d26993
jadesoul <wslgb2006@gmail.com>
2012-07-07 03:15:32 +0800

    mod the setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
+from distutils import sysconfig
 from distutils.core import setup
-from os import listdir
+from os import listdir, getcwd
 
+# generate pth file
+f=open('libjade.pth', 'w')
+f.write(getcwd())
+f.close()
+
+# setup
+site_packages_path=sysconfig.get_python_lib()
 setup(
 	name='libjade',
 	version='1.0',
@@ -10,7 +18,7 @@ setup(
 	author_email='wslgb2006@gmail.com',
 	url='http://jadesoul.sinaapp.com/',
 	license='Python Software Foundation License',
-	packages=['libjade'],
+	# packages=['libjade'], # install a pth file instead of a package for debug
 	scripts=['scripts/'+i for i in listdir('scripts')],
+	data_files=[(site_packages_path, ["libjade.pth"])]
 )
-
