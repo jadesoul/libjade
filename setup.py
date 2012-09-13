60a29bef91a993b03551ddbd59a64f461c96345d
jadesoul <wslgb2006@gmail.com>
2012-09-13 16:37:58 +0800

    change back setup.py to install all code Signed-off-by: jadesoul <wslgb2006@gmail.com>

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@ from distutils.core import setup
 from os import listdir, getcwd
 from os.path import isfile
 
-# generate pth file
-f=open('libjade.pth', 'w')
-f.write(getcwd())
-f.close()
-
-# setup
-site_packages_path=sysconfig.get_python_lib()
 setup(
 	name='libjade',
 	version='1.0',
@@ -19,7 +12,6 @@ setup(
 	author_email='wslgb2006@gmail.com',
 	url='http://jadesoul.sinaapp.com/',
 	license='Python Software Foundation License',
-	# packages=['libjade'], # install a pth file instead of a package for debug
+	packages=['libjade'], 
 	scripts=filter(isfile, ['scripts/'+i for i in listdir('scripts')]),
-	data_files=[(site_packages_path, ["libjade.pth"])]
 )
