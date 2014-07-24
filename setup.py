fce95879dcaddc376a41db1766030174326153f0
jadesoul <wslgb2006@gmail.com>
2014-07-24 10:39:57 +0800

    change libjade as install package

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
-from distutils import sysconfig
 from distutils.core import setup
-from os import listdir, getcwd
-from os.path import isfile
 
-# generate pth file
-f=open('libjade.pth', 'w')
-f.write(getcwd())
-f.close()
-
-# setup
-site_packages_path=sysconfig.get_python_lib()
 setup(
 	name='libjade',
 	version='1.0',
@@ -19,6 +9,5 @@ setup(
 	author_email='wslgb2006@gmail.com',
 	url='http://jadesoul.sinaapp.com/',
 	license='Python Software Foundation License',
-	scripts=filter(isfile, ['scripts/'+i for i in listdir('scripts')]),
-	data_files=[(site_packages_path, ["libjade.pth"])]
+	packages=['libjade'],
 )
